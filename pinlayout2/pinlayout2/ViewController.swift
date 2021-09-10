11352e14e2ebca4107d2fe1ae5f903adaee39fb2
caelumturc <87136151+caelumturc@users.noreply.github.com>
2021-09-10 09:20:10 -0700

    Add files via upload

---
+++ pinlayout2/pinlayout2/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  pinlayout2
+//
+//  Created by goktan on 2.09.2021.
+//  Copyright © 2021 caelum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ pinlayout2/pinlayout2Tests/pinlayout2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  pinlayout2Tests.swift
+//  pinlayout2Tests
+//
+//  Created by goktan on 4.07.2021.
+//  Copyright © 2021 caelum. All rights reserved.
+//
+
+import XCTest
+@testable import pinlayout2
+
+class pinlayout2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
